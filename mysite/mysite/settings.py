fb3bf3498cc02936a0678b9e4e9249f6b0619190
Gamer4life <lpuvilla0001@mymail.lausd.net>
2020-01-29 22:51:24 +0000

    Added stuff

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,9 +29,9 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = 'o9#8zw-#ni&11_l!mml8#t%f_7t8$+l1%gr=x1g!xw+*_hbi30'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
